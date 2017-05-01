992c45a4a18d7a714f299c506389f08c1826ad67
Minnal Kunnan <minnalkunnan@gmail.com>
2017-05-01 13:16:08 -0700

    Working server with wsgi

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a proof of concept for k-anonymity and differential privacy done for CPE 429 Advanced Topics in Computer Security
---
+++ myproject.py
@@ -0,0 +1,15 @@
+#!flask/bin/python
+from flask import Flask, jsonify, abort, request, make_response, render_template
+
+app = Flask(__name__, static_url_path = "/static", static_folder = "static")
+
+@app.route('/')
+@app.route('/index')
+def index():
+	print(request)
+	f = open('workfile', 'a')
+	f.write(str({"newHeader": str(request.headers)}))
+	return render_template('index.html', title='Home')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
